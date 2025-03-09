aba33280ee101a85141a7514e26f3fe2816fc62d
Dikozda <dkozda@gmail.com>
2025-03-09 11:45:32 +1100

    cap framerate to 60 and keep track of delta time

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print (f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     game = True
     while game:
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
 
         screen.fill(000)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
